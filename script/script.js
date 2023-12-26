fbe9ed95acdeaf420a285149d506ea62c92e234f
Sudi Mwakimako <91537436+sudijuma@users.noreply.github.com>
2023-12-27 00:27:38 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,2 +1,5 @@
 const nasaImage = document.querySelector(".imageContainer");
 console.log(nasaImage);
+
+const nasaApiUrl = "https://api.nasa.gov/planetary/apod";
+const apiKey = "bQda8zxBMGQUcbPIvQRKVczRhCkq1bJJDy5Jiq6V";
